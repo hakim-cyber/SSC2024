aa04590ad67baf4b9b71f50a611a76c5ad922884
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2024-01-11 02:06:53 +0400

    added water fall

--- Pythagorean Teorem Model/PythagoreanAbout.swift
+++ Pythagorean Teorem Model/PythagoreanAbout.swift
@@ -12,11 +12,15 @@ import SwiftUI
 struct PythagoreanAbout: View {
     @State private var percentA = 105.0
     @State private var percentB = 105.0
-    @State private var percentC = -10.0
+    @State private var percentC =  -10.0
     
     @State private var rotate = -40.0
+    
+    @State private var animate = false
+    
+    @State private var showWaterFall = false
     var body: some View {
-        HStack{
+        
             VStack {
                         Triangle()
                    
@@ -24,13 +28,13 @@ struct PythagoreanAbout: View {
                    
                             .overlay(
                                 ZStack{
-                                    WaveAnimationRectangle(percent:percentC, name: "c" )
+                                    WaveAnimationRectangle(showWaterFall:showWaterFall, percent:percentC, name: "c" )
                                         .frame(width:200, height: 200)
                                      
                                         .rotationEffect(.degrees(210))
                                         .offset(x: 55, y: -90)
                                     
-                                    WaveAnimationRectangle(percent:percentB, name: "b" )
+                                    WaveAnimationRectangle(showWaterFall: false, percent:percentB, name: "b" )
                                    
                                          .frame(width:100 * CGFloat(sqrt(3)), height: 100 * CGFloat(sqrt(3)))
                                       .rotationEffect(.degrees(180))
@@ -38,7 +42,7 @@ struct PythagoreanAbout: View {
                                          .offset(x: 0, y: 145)
                                         
                                     
-                                    WaveAnimationRectangle(percent:percentA ,name: "a")
+                                    WaveAnimationRectangle(showWaterFall: false, percent:percentA ,name: "a")
                                          .frame(width:100 , height: 100)
                                       .rotationEffect(.degrees(180))
                                          .rotationEffect(.degrees(90 ))
@@ -58,27 +62,61 @@ struct PythagoreanAbout: View {
                     .padding()
                     .onAppear(perform: {
                         
-                        withAnimation(.easeInOut(duration: 5.5)) {
-                            
-                            self.rotate = 150
-                            
-                        }
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5){
-                            withAnimation(.bouncy(duration: 5.5)){
-                                self.percentA = -10.0
-                                self.percentC = 99.0 / CGFloat(sqrt(3))
-                            }
-                        }
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5){
-                            withAnimation(.bouncy(duration: 5.5)){
-                                self.percentB = -10.0
-                                self.percentC = 105.0
-                            }
-                        }
+                        self.animate = true
                          
                     })
                     .scaleEffect(1)
-        }
+                    .onChange(of: animate) {_, newValue in
+                        if newValue{
+                            
+                            withAnimation(.easeInOut(duration: 5.5)) {
+                                
+                                self.rotate = 150
+                                
+                            }completion: {
+                                self.showWaterFall = false
+                                self.animate = false
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5){
+                                withAnimation(.bouncy(duration: 5.5)){
+                                    self.showWaterFall = true
+                                    self.percentA = -10.0
+                                    self.percentC = 99.0 / CGFloat(sqrt(3))
+                                }
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5){
+                                withAnimation(.bouncy(duration: 5.5)){
+                                    self.percentB = -10.0
+                                    self.percentC = 105.0
+                                }
+                            }
+                        }else{
+                            withAnimation(.easeInOut(duration: 0.5)) {
+                                self.rotate = -40
+                                self.percentA = 105
+                                self.percentC = -10
+                                self.percentB = 105
+                            }
+                        }
+                    }
+                    
+                    .frame(width:UIScreen.main.bounds.width / 2,height: UIScreen.main.bounds.height / 2)
+                    .overlay(alignment: .topTrailing){
+                        if !animate {
+                          
+                                Button{
+                                    self.animate = true
+                                }label: {
+                                    Image(systemName: "play.circle")
+                                        .bold()
+                                        .font(.system(size: 60))
+                                }
+                            }
+                            
+                        
+                    }
+                    
+                    
     }
 }
 /*
@@ -99,28 +137,62 @@ struct Triangle:Shape{
         return p
     }
 }
+struct CurvedLine:Shape{
+    let reverse:Bool
+    func path(in rect: CGRect) -> Path {
+        var p = Path()
+        if !reverse{
+            p.move(to: .init(x: rect.maxX, y: rect.minY))
+            p.addCurve(to:.init(x: rect.midX + 20, y: rect.maxY), control1: CGPoint(x: rect.midX + 30, y: rect.midY - 40), control2: CGPoint(x: rect.midX , y: rect.maxY))
+        }else{
+            p.move(to: .init(x: rect.minX, y: rect.minY))
+            p.addCurve(to:.init(x: rect.midX  - 20, y: rect.maxY), control1: CGPoint(x: rect.midX - 30, y: rect.midY - 40), control2: CGPoint(x: rect.midX , y: rect.maxY))
+        }
+        return p
+    }
+}
 
 struct WaveAnimationRectangle: View {
-    
+    let showWaterFall:Bool
     let percent :Double
     let name:String
     @State private var waveOffset = Angle(degrees: 0)
     
     var body: some View {
-        ZStack {
+        GeometryReader{geo in
            
-            Wave(offSet: Angle(degrees: waveOffset.degrees), percent: percent)
-                .fill(Color.blue)
-                .ignoresSafeArea(.all)
-                .padding(1.5)
-           
-            Rectangle().stroke(lineWidth: 8)
+            ZStack{
+                if  showWaterFall{
+                    CurvedLine(reverse: false)
+                        .stroke(.blue,style: .init(lineWidth: showWaterFall ? 6 :0))
+                        .frame(width:geo.size.width  , height:geo.size.height)
+                        .transition(.asymmetric(insertion: .scale(scale:0.75).animation(.easeInOut(duration: 0.7)), removal: .identity))
+                    CurvedLine(reverse: true)
+                        .stroke(.blue,style: .init(lineWidth: showWaterFall ? 6 :0))
+                        .frame(width:geo.size.width  , height:geo.size.height)
+                        .transition(.asymmetric(insertion: .scale(scale:0.75).animation(.easeInOut(duration: 0.7)), removal: .identity))
+                       
+                }
                 
+                Wave(offSet: Angle(degrees: waveOffset.degrees), percent: percent)
+                    .fill(Color.blue)
+                    .ignoresSafeArea(.all)
+                    .padding(1.5)
+                    .frame(width:geo.size.width , height:geo.size.height)
+                
+                
+                Rectangle().stroke(lineWidth: 8)
+                    .frame(width:geo.size.width , height:geo.size.height)
+                
+            }
+            .frame(maxWidth: .infinity,maxHeight: .infinity,alignment: .center)
+            
         }
+        
         .overlay(content: {
             HStack(spacing:1){
                 Text(name)
-                  
+                
                     .font(.system(size: 25))
                 Text("2")
                     .font(.system(size: 15))
