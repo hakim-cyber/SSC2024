0bd18859e8ab4ac1aee20c953d0a8edbbc79b535
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2024-01-20 20:26:03 +0400

    Nice

---
+++ Pythagorean Teorem Model/PythagoreanSimulatorAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by aplle on 1/20/24.
+//
+
+import SwiftUI
+
+struct PythagoreanSimulatorAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PythagoreanSimulatorAnimation()
+}
