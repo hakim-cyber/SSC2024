e5dd5bdc2353c41e4b7df55f2c4db9f307c76553
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2024-01-25 00:00:33 +0400

    made complex vessels

--- CommunicatingVessels/CommunicatingVessel.swift
+++ CommunicatingVessels/CommunicatingVessel.swift
@@ -23,12 +23,13 @@ struct CommunicatingVessel: View {
                         
                     Vessel2()
                         .fill(Color.brown)
+                        .stroke(Color.white.opacity(0.8), lineWidth: 5)
                         .colorMultiply(.gray)
                    
                 }
-                    .frame(width: UIScreen.main.bounds.width,height: 400)
+                .frame(width: UIScreen.main.bounds.width + 5,height: 400)
                     .clipped()
-                   
+                    .offset(y:5)
             }
             
         }
@@ -52,14 +53,14 @@ struct CommunicatingVessel: View {
 struct Vessel1:Shape{
     func path(in rect: CGRect) -> Path {
         var p = Path()
-        p.move(to: .zero)
-        p.addLine(to: CGPoint(x: rect.width * 0.2, y: .zero ))
+        p.move(to:  CGPoint(x: rect.width * 0.2, y: .zero ))
+       
         p.addLine(to: CGPoint(x: rect.width * 0.1, y: rect.height * 0.75 ))
         
         p.addLine(to: CGPoint(x: rect.width * 0.8, y: rect.height * 0.75 ))
         
         p.addLine(to: CGPoint(x: rect.width * 0.9, y: .zero  ))
-        p.addLine(to: CGPoint(x: rect.maxX , y: .zero  ))
+        p.move(to: CGPoint(x: rect.maxX , y: .zero  ))
         
         p.addLine(to: CGPoint(x: rect.maxX , y: rect.maxY  ))
         p.addLine(to: CGPoint(x: rect.minX , y: rect.maxY  ))
@@ -73,7 +74,7 @@ struct Vessel1:Shape{
         p.addLine(to: CGPoint(x: rect.width * 0.575, y: rect.height * 0.25 * 2.5))
         p.addLine(to: CGPoint(x: rect.width * 0.5, y: .zero))
         
-        p.addLine(to: CGPoint(x: rect.width * 0.3, y: .zero))
+       
         
         return p
     }
@@ -85,8 +86,8 @@ struct Vessel2:Shape{
     func path(in rect: CGRect) -> Path {
         var p = Path()
         
-        p.move(to: .zero)
-        p.addLine(to: CGPoint(x: rect.width * 0.15, y: .zero ))
+        p.move(to: CGPoint(x: rect.width * 0.15, y: .zero ))
+       
         
         p.addLine(to: CGPoint(x: rect.width * 0.15, y: rect.height * 0.75))
         
@@ -96,13 +97,37 @@ struct Vessel2:Shape{
         p.addLine(to: CGPoint(x: rect.width * 13 / 16, y: rect.height * 0.30 ))
         p.addLine(to: CGPoint(x: rect.width * 7 * 0.125, y: rect.height * 0.15 ))
         p.addLine(to: CGPoint(x: rect.width * 13 / 16, y: .zero ))
-        p.addLine(to: CGPoint(x: rect.maxX, y: .zero))
+        
+        
+        p.move(to: CGPoint(x: rect.maxX, y: .zero))
         p.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
         
         p.addLine(to: CGPoint(x:.zero, y: rect.maxY))
         
         p.addLine(to:.zero)
         
+        
+        p.move(to: CGPoint(x: rect.width * 0.25, y: .zero))
+        p.addLine(to: CGPoint(x: rect.width * 0.25, y: rect.height * 0.6))
+        p.addLine(to: CGPoint(x: rect.width * 5 / 16, y: rect.height * 0.6))
+        p.addLine(to: CGPoint(x: rect.width * 5 / 16, y: .zero))
+        
+        
+        p.move(to: CGPoint(x: rect.width * 6 / 16, y: .zero))
+        p.addLine(to: CGPoint(x: rect.width * 6 / 16, y: rect.height * 0.6))
+        p.addLine(to: CGPoint(x: rect.width * 7.5 / 16, y: rect.height * 0.6))
+        p.addLine(to: CGPoint(x: rect.width * 9.5 / 16, y: .zero))
+        
+        p.move(to: CGPoint(x: rect.width * 11 / 16, y: .zero))
+        p.addLine(to: CGPoint(x: rect.width * 9 / 16, y: rect.height * 0.6))
+        p.addLine(to: CGPoint(x: rect.width * 11.75 / 16, y: rect.height * 0.6))
+       
+        p.addLine(to: CGPoint(x:  rect.width * 12.75 / 16, y: rect.height * 0.45 ))
+        p.addLine(to: CGPoint(x:  rect.width * 11.75 / 16, y: rect.height * 0.30 ))
+        p.addLine(to: CGPoint(x: rect.width * 12.75 / 16, y: rect.height * 0.15 ))
+        
+        p.addLine(to: CGPoint(x: rect.width * 11.75 / 16, y: .zero))
+       
         
         return p
     }
